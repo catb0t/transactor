557ad8e37be5a7e680ab960b773217b0b695512c
Cat Stevens <catb0t@protonmail.ch>
2018-01-17 18:42:20 -0500

    use a deque_pool instead, which is like a PriorityQueue

--- transactor.py
+++ transactor.py
@@ -1,5 +1,23 @@
 from collections import deque
+import enum
 import time
+
+
+class priority(enum.IntEnum):
+    undef  = 0
+    low    = 1
+    normal = 2
+    high   = 3
+    airmail = 4
+
+
+DEFAULT_MAXLENS = {
+    priority.undef:   None,
+    priority.low:     None,
+    priority.normal:  None,
+    priority.high:    50,
+    priority.airmail: 10
+}
 
 
 def microtime():
@@ -18,6 +36,10 @@ def _static_vars(**kwargs):
             setattr(func, k, kwargs[k])
         return func
     return decorate
+
+
+def get_maxlen(params, key):
+    return params.get(key, DEFAULT_MAXLENS[key])
 
 
 @_static_vars(index=0)
@@ -52,15 +74,80 @@ class hashable_dict(dict):
         return hash(hashstr)
 
 
+class deque_pool():
+    @staticmethod
+    def default_nice_sorter(nices):
+        return sorted(nices, key=lambda x: x.value, reverse=True)
+
+    def __init__(self, *args, **kwargs):
+        self.pool = {
+            priority.undef:
+                deque(maxlen=get_maxlen(kwargs, priority.undef)),
+            priority.low:
+                deque(maxlen=get_maxlen(kwargs, priority.low)),
+            priority.normal:
+                deque(maxlen=get_maxlen(kwargs, priority.normal)),
+            priority.high:
+                deque(maxlen=get_maxlen(kwargs, priority.high)),
+            priority.airmail:
+                deque(maxlen=get_maxlen(kwargs, priority.airmail))
+        }
+
+    def push(
+        self, obj,
+        nice=priority.normal,
+        push_func=lambda q, o: q.appendleft(o)
+    ):
+        if self._can_push(nice):
+            push_func(self.pool[nice], obj)
+            return nice
+
+        while not self._can_push(nice) and (nice >= priority.undef):
+            nice -= 1
+        push_func(self.pool[nice], obj)
+        return nice
+
+    def pop(self):
+        return self.pop_nice()
+
+    def pop_nice(
+        self,
+        nice_sorter=default_nice_sorter,
+        pop_func=lambda q: q.pop()
+    ):
+        nices = self.sort_pool(nice_sorter)
+        for ni in nices:
+            dq = self.pool[ni]
+            if len(dq):
+                return pop_func(dq)
+        return None
+
+    def sort_pool(self, nice_sorter=default_nice_sorter):
+        return nice_sorter( self.pool.keys() )
+
+    def pop_random(self):
+        import random
+        return self.sort_pool( lambda x: random.sample(x, len(x)) )
+
+    def _can_push(self, nice):
+        if None is not self.pool[nice].maxlen:
+            return len( self.pool[nice] ) < self.pool[nice].maxlen
+        return True
+
+
 class request_clerk():
 
     def __init__(self, *args, **kwargs):
-        # read and write request pool
-        self._requests  = deque()
+        '''
+            Base class for request pool management objects.
+        '''
+        # read and write request pool (list?)
+        self._requests  = deque_pool()
+        # impl detail, temp record of recent uuids
         self._known_uuids = set()
         # used only by read subclass; dict: uuid -> data
         self._responses = dict()
-        # status codes etc; dict: uuid -> rank, ok?, completed time
+        # descriptions; dict: uuid -> rank, ok?, completed time
         self._descrs    = dict()
 
 # "public" API
@@ -68,8 +155,23 @@ class request_clerk():
 # pre-work (introduction)
 
     def impl_register_request(self, req, prefunc=lambda x: x):
-        self._known_uuids.add(req["uuid"])
-        self._requests.appendleft(req)
+        '''
+            Arguments:  req (a dict) and prefunc (a function x -> g)
+            Returns:    None
+            Throws:     KeyError if req is missing
+                        "uuid", "nice" or "rank" keys
+            Effects:    modifies _known_uuids and _requests, extending them
+
+            Implementation detail to register a request in the pool.
+
+            req[uuid] is registered as a known uuid.
+            req is pushed to the end of the request pool based on its rank
+        '''
+        # TODO: rank
+        req = prefunc(req)
+        rank = req.get("rank", req.get("nice", 20))
+        self._known_uuids.add( req["uuid"] )
+        self._requests.append_left_ranked(req, rank)
 
 # post-work (epilogue)
 
@@ -118,7 +220,6 @@ class request_clerk():
             resp = self.impl_ul_get_response()
         return resp
 
-
 # "background" API
 
     def impl_have_own_uuid(self, uuid):
@@ -127,12 +228,21 @@ class request_clerk():
 # arbiter introduction
 
     def impl_pop_request(self):
+        # top (right side) of the deque
         return self._requests.pop(), microtime()
+
+# arbiter impl
+
+    def a(self):
+        pass
 
 # arbiter epilogue
 
-    def impl_record_descr(self, res):
-        self.descrs[ res["uuid"] ] = res
+    def impl_set_descr(self, descr):
+        self._descrs[ descr["uuid"] ] = descr
+
+    def impl_set_response(self, uuid, data):
+        self._responses[uuid] = data
 
 
 class write_clerk(request_clerk):
